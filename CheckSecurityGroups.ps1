0d05dfe8b38f7487aaf695c1f28428679332d8ae
derks-wesley <90984101+derks-wesley@users.noreply.github.com>
2023-01-26 12:18:19 +0100

    Update CheckSecurityGroups.ps1

--- CheckSecurityGroups.ps1
+++ CheckSecurityGroups.ps1
@@ -4,7 +4,8 @@
 
 .DESCRIPTION
     This simple script checks which users are added to which security groups on Windows. After checking it will generate a .CSV file for easy reading.
-    If requested i will add the function to exclude certain groups, or you can add it youself. Did not add it because there was no need for it in my situation. 
+    If requested i will add the function to exclude certain groups, or you can add it youself. Did not add it because there was no need for it in my situation.
+    If you edit it yourself, kindly share what you did so we can help each other.
 
 .EXAMPLE
     Example syntax for running the script or function.
